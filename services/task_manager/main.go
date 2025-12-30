3d7260c7cc55e76eb577e1add5b906d7705712c3
Uladzislau <shishko.v@promail366.com>
2025-12-31 01:40:33 +0300

    servie-creation: start creating service task_manager

--- services/task_manager/main.go
+++ services/task_manager/main.go
@@ -1,5 +1,46 @@
-package main
+package task_manager
 
-func main() {
+import "time"
 
+type Task struct {
+	title       string
+	description string
+	createdAt   time.Time
+	expiresAt   time.Time
+	softDeleted bool
+}
+
+type DB interface {
+	SaveTask(task *Task) (*Task, error)
+	SoftDeleteTask(task *Task) (*Task, error)
+	UpdateTask(task *Task) (*Task, error)
+}
+
+type TaskManager struct {
+	db DB
+}
+
+func NewTaskManager(db *DB) *TaskManager {
+	return &TaskManager{db: db}
+}
+
+func (t *TaskManager) CreateTask(task *Task) (*Task, error) {
+	if _, err := t.db.SaveTask(task); err != nil {
+		return nil, err
+	}
+	return task, nil
+}
+
+func (t *TaskManager) DeleteTask(task *Task) (*Task, error) {
+	if _, err := t.db.SoftDeleteTask(task); err != nil {
+		return nil, err
+	}
+	return task, nil
+}
+
+func (t *TaskManager) UpdateTask(task *Task) (*Task, error) {
+	if _, err := t.db.SaveTask(task); err != nil {
+		return nil, err
+	}
+	return nil, nil
 }
